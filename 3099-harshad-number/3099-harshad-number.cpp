29a14daa02cbbc733a022ed2892c3e8dfe279f5f
Ankit Parida <ankitparida386@gmail.com>
2025-11-25 19:22:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.77%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0,temp=x;
+        while(temp>0){
+            int b=temp%10;
+            sum+=b;
+            temp/=10;
+        }
+        if(x%sum==0){
+            return sum;
+        }
+        else{
+            return -1;
+        }
+    }
+};
