1106ba256188360fcfa07987d3b51b352ab326c0
Ankit Parida <ankitparida386@gmail.com>
2025-12-07 15:46:14 +0530

    Time: 0 ms (100%), Space: 47.1 MB (43.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-       int n=nums.length;
-       int totalsum=n*(n+1)/2,sum=0;
-       for(int i=0;i<n;i++){
-        sum+=nums[i];
-       }
-       int result=totalsum-sum;
-       return result;
+      int n=nums.length;
+      int sum=n*(n+1)/2;
+      int total=0;
+      for(int i=0;i<n;i++){
+         total+=nums[i];
+      }
+      int result=sum-total;
+      return result;
     }
 }
