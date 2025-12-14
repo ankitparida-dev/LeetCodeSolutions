5e9651d9fb203141ac46f1f64c77f1efbde06f4a
Ankit Parida <ankitparida386@gmail.com>
2025-12-14 08:46:42 +0530

    Time: 1 ms (62.34%), Space: 42.1 MB (58.45%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-          int count=0;
-          while(num1!=0 && num2!=0){
+         int count=0;
+         while(num1!=0 && num2!=0){
             if(num1>=num2){
                 num1-=num2;
             }
@@ -9,7 +9,7 @@ class Solution {
                 num2-=num1;
             }
             count++;
-          }
-          return count;
+         }
+         return count;
     }
 }
