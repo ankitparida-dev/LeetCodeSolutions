e1a9cc2c1039156b39511186a64600afd0e94a73
Ankit Parida <ankitparida386@gmail.com>
2025-12-30 14:24:28 +0530

    Time: 0 ms (100%), Space: 46.8 MB (6.94%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]!=matrix[i-1][j-1]){
-                    return false;
-                }
-                }
-            
+       int r=matrix.length;
+       int c=matrix[0].length;
+       for(int i=1;i<r;i++){
+        for(int j=1;j<c;j++){
+            if(matrix[i][j]!=matrix[i-1][j-1]){
+                return false;
+            }
         }
-        return true;
+       }
+       return true;
     }
 }
