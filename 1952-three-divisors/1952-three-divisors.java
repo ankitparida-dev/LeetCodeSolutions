311cf76da6fa0c168571d675564f3be012e4f187
Ankit Parida <ankitparida386@gmail.com>
2025-12-30 10:34:21 +0530

    Time: 1 ms (77.17%), Space: 42.2 MB (51.63%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean isThree(int n) {
-       int count=0;
-       for(int i=1;i<=n;i++){
-        if(n%i==0){
-            count++;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
         }
-       }
-       if(count==3){
-        return true;
-       }
-       else{
+        if(count==3){
+            return true;
+        }
         return false;
-       }
     }
 }
