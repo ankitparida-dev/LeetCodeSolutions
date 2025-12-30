89117f392f310d81a7ceb6574abb51a25c9794d5
Ankit Parida <ankitparida386@gmail.com>
2025-12-30 13:49:00 +0530

    Time: 1 ms (62.3%), Space: 42.2 MB (36.44%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,12 +1,12 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int highest=a>b?a:b;
-        int count=0;
-        for(int i=1;i<=highest;i++){
-            if(a%i==0 && b%i==0){
-                count++;
-            }
+       int highest=a>b?a:b;
+       int count=0;
+       for(int i=1;i<=highest;i++){
+        if(a%i==0 && b%i==0){
+            count++;
         }
-        return count;
+       }
+       return count;
     }
 }
