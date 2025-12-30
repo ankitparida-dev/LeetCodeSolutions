dd81df5c2025feeb3e385d7258e3bd3cc0912d65
Ankit Parida <ankitparida386@gmail.com>
2025-12-30 09:47:49 +0530

    Time: 0 ms (100%), Space: 42.3 MB (11.56%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n%2==0){
-            return true;
-        }
-        else{
-            return false;
-        }
+      if(n%2==0){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
 }
