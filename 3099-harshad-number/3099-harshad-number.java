0514168bae156bc7da38875c6716dde9d32f7f29
Ankit Parida <ankitparida386@gmail.com>
2025-12-30 15:13:00 +0530

    Time: 0 ms (100%), Space: 41.9 MB (75.9%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+          int sum=0;
+          int temp=x;
+          while(temp>0){
+            int b=temp%10;
+            sum+=b;
+            temp/=10;
+          }
+          if(x%sum==0){
+            return sum;
+          }
+          return -1;
+    }
+}
