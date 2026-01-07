44cd4edf97c10848a8b6e0bcf319493e5ad82263
Ankit Parida <ankitparida386@gmail.com>
2026-01-07 19:10:40 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,10 +3,21 @@ class Solution {
         int count0 = 0, count1 = 0, count2 = 0;
 
        
-        for (int num : nums) {
-            if (num == 0) count0++;
-            else if (num == 1) count1++;
-            else count2++;
+        // for (int num : nums) {
+        //     if (num == 0) count0++;
+        //     else if (num == 1) count1++;
+        //     else count2++;
+        // }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }
         }
 
         
