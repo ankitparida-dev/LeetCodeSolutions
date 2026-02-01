24d8031dc693da4e2c8a809dda8b38f7ff0aae68
Ankit Parida <ankitparida386@gmail.com>
2026-02-02 04:51:30 +0530

    Time: 0 ms (100%), Space: 46.3 MB (19.26%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,8 +1,7 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double k=celsius+273.15;
-        double f=celsius*1.80+32.00;
-        double temp[]={k,f};
-        return temp;
+        double kelvin=celsius+273.15;
+        double fahrenheit=celsius*1.80+32.00;
+        return new double[]{kelvin,fahrenheit};
     }
 }
