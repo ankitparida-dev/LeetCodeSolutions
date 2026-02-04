8291a8db3a61e90d795f704e511dd83630ca39e3
Ankit Parida <ankitparida386@gmail.com>
2026-02-04 05:41:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.43%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.java
+++ 3099-harshad-number/3099-harshad-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-          int sum=0;
-          int temp=x;
-          while(temp>0){
-            int b=temp%10;
-            sum+=b;
+        int sum=0,temp=x;
+        while(temp>0){
+            sum+=temp%10;
             temp/=10;
-          }
-          if(x%sum==0){
+        }
+        if(x%sum==0){
             return sum;
-          }
-          return -1;
+        }
+        else{
+            return -1;
+        }
     }
 }
