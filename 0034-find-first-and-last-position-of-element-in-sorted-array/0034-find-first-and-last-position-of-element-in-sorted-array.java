d9828da977985bdeb43ed30746977b9ee2ee68b6
Ankit Parida <ankitparida386@gmail.com>
2026-03-03 19:40:03 +0530

    Time: 0 ms (100%), Space: 48 MB (87.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first=firstoccurene(nums,target);
-        int last=lastoccurence(nums,target);
-        int result[]=new int[]{first,last};
+      int result1=firstOccurence(nums,target);
+      int result2=lastOccurence(nums,target);
+      return new int[]{result1,result2};
+    }
+    public static int firstOccurence(int []nums,int target){
+        int low=0,high=nums.length-1;
+        int result=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+               result= mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
         return result;
     }
-    private int firstoccurene(int[] nums,int target){
-       int n=nums.length;
-       int low=0;
-       int high=n-1;
-       int index=-1;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target){
-            index=mid;
-            high=mid-1;
+     public static int lastOccurence(int []nums,int target){
+        int low=0,high=nums.length-1;
+        int result=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                result= mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        else if(nums[mid]<target){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-       }
-       return index;
-    }
-    private int lastoccurence(int[] nums,int target){
-       int n=nums.length;
-       int low=0;
-       int high=n-1;
-       int index=-1;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target){
-            index=mid;
-            low=mid+1;
-        }
-        else if(nums[mid]<target){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-       }
-       return index;
+        return result;
     }
 }
