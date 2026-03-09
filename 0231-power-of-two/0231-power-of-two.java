17e2f2dcbe97de336960e1077696e1c5bd3fc642
Ankit Parida <ankitparida386@gmail.com>
2026-03-09 05:35:33 +0530

    Time: 0 ms (100%), Space: 42.5 MB (73.3%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if((n&(n-1))==0&&(n>0)){
-            return true;
-        }
-        return false;
+       if((n>0)&&(n&(n-1))==0){
+        return true;
+       }
+       return false;
     }
 }
