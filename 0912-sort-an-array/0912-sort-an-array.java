fa43b5489a46fd214593e301e373e040cf7c1a05
Ankit Parida <ankitparida386@gmail.com>
2026-03-23 06:10:34 +0530

    Time: 26 ms (61.18%), Space: 86.5 MB (28.82%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -3,28 +3,7 @@ class Solution {
         mergeSort(nums,0,nums.length-1);
         return nums;
     }
-    public void merge(int []nums,int l,int m,int r){
-        int temp[]=new int[r-l+1];
-        int i=l,j=m+1,k=0;
-        while(i<=m && j<=r){
-            if(nums[i]<=nums[j]){
-                temp[k++]=nums[i++];
-            }
-            else{
-                temp[k++]=nums[j++];
-            }
-        }
-        while(i<=m){
-            temp[k++]=nums[i++];
-        }
-        while(j<=r){
-            temp[k++]=nums[j++];
-        }
-        for(i=l,k=0;i<=r;i++,k++){
-            nums[i]=temp[k];
-        }
-    }
-    public void mergeSort(int []nums,int l,int r){
+    public void mergeSort(int nums[],int l,int r){
         if(l>=r){
             return;
         }
@@ -32,5 +11,26 @@ class Solution {
         mergeSort(nums,l,mid);
         mergeSort(nums,mid+1,r);
         merge(nums,l,mid,r);
+    }
+    public void merge(int nums[],int l,int mid,int r){
+        int temp[]=new int[r-l+1];
+        int i=l,j=mid+1,k=0;
+        while(i<=mid && j<=r){
+            if(nums[i]<=nums[j]){
+               temp[k++]=nums[i++];
+            }
+            else{
+                temp[k++]=nums[j++];
+            }
+        }
+        while(i<=mid){
+             temp[k++]=nums[i++];
+        }
+        while(j<=r){
+            temp[k++]=nums[j++];
+        }
+        for(i=l,k=0;i<=r;i++,k++){
+            nums[i]=temp[k];
+        }
     }
 }
