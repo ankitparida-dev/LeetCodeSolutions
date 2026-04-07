f9498a153f889ad548fe9b19bd1d63eadcfccbb8
Ankit Parida <ankitparida386@gmail.com>
2026-04-07 09:04:03 +0530

    Time: 3 ms (87.61%), Space: 43.2 MB (63.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean isValid(String s) {
         Stack <Character> stack=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='(' || ch=='{' || ch=='['){
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
                 stack.push(ch);
             }
             else{
@@ -10,7 +11,7 @@ class Solution {
                     return false;
                 }
                 char top=stack.pop();
-                if((ch==')' && top!='(')||(ch=='}' && top!='{')||(ch==']' && top!='[')){
+                if((ch==']' && top!='[')||(ch=='}' && top!='{')||(ch==')' && top!='(')){
                     return false;
                 }
             }
