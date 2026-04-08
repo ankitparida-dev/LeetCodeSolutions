a6237f22462dc1bbf1c8328b3b94030197e8c83d
Ankit Parida <ankitparida386@gmail.com>
2026-04-08 09:28:21 +0530

    Time: 14 ms (31.59%), Space: 46 MB (36.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        return solve(s, 0, s.length() - 1);
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int low=0,high=s.length()-1;
+        while(low<=high){
+            if(s.charAt(low)!=s.charAt(high)){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
     }
 
-    private boolean solve(String s, int i, int j) {
-       
-        if (i >= j) return true;
-
-        if (!Character.isLetterOrDigit(s.charAt(i))) {
-            return solve(s, i + 1, j);
-        }
-        if (!Character.isLetterOrDigit(s.charAt(j))) {
-            return solve(s, i, j - 1);
-        }
-
-        if (Character.toLowerCase(s.charAt(i)) != 
-            Character.toLowerCase(s.charAt(j))) {
-            return false;
-        }
-
-        // move अंदर
-        return solve(s, i + 1, j - 1);
-    }
+   
 }
