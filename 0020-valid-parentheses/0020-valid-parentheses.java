9caf266c371e548d32c49b7ac4e203a3ad9bf93b
Ankit Parida <ankitparida386@gmail.com>
2026-05-27 14:22:36 +0530

    Time: 3 ms (88.04%), Space: 43.1 MB (71.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack <Character> stack=new Stack<>();
-       for(int i=0;i<s.length();i++){
-        char c=s.charAt(i);
-        if(c=='(' || c=='{' ||c=='['){
-            stack.push(c);
-        }
-        else if(c==')' || c=='}' ||c==']'){
-            if(stack.isEmpty()){
-                return false;
+        Stack <Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='{' || c=='[' || c=='('){
+                stack.push(c);
             }
-            else{
-                char top=stack.pop();
-                if((c==')' && top!='(')||(c=='}' && top!='{')||(c==']' && top!='[')){
+            else if(c=='}' || c==']' || c==')'){
+                if(stack.isEmpty()){
                     return false;
+                }
+                else{
+                    char top=stack.pop();
+                    if((c=='}' && top!='{') || (c==')' && top!='(') || (c==']' && top!='[')){
+                        return false;
+                    }
                 }
             }
         }
-       }
-       return stack.isEmpty();
+        return stack.isEmpty();
     }
 }
