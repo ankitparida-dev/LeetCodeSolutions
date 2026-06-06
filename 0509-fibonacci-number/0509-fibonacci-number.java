0452054ac9604a95269b1cefdfd92093a1e3b638
Ankit Parida <ankitparida386@gmail.com>
2026-06-06 18:24:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (32.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,9 @@
 class Solution {
-    public static int helper(int n,int a,int b){
+    public int helper(int n,int a,int b){
         if(n==0){
             return a;
         }
-      return helper(n-1,b,a+b);
+        return helper(n-1,b,a+b);
     }
     public int fib(int n) {
        return helper(n,0,1);
