568feb751566e7352361c3698e58b03e289ca23b
Ankit Parida <ankitparida386@gmail.com>
2026-06-23 23:17:07 +0530

    Time: 1 ms (99.98%), Space: 42.6 MB (63.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,13 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int rev = 0,temp=x;
+        int rev=0;
         while(x!=0){
             int b=x%10;
             x/=10;
-             if (rev > 214748364 || rev < -214748364)
+            if(rev>214748364 || rev<-214748364){
                 return 0;
+            }
             rev=rev*10+b;
-            
         }
         return rev;
     }
