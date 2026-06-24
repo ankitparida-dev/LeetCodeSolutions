7d9e4d933e6fe8d57eace8caa384dd576855abaf
Ankit Parida <ankitparida386@gmail.com>
2026-06-24 08:42:44 +0530

    Time: 0 ms (100%), Space: 42 MB (53.4%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canWinNim(int n) {
-      if(n%4!=0){
-        return true;
-      }
-      return false;
+        if(n%4!=0){
+            return true;
+        }
+        return false;
     }
 }
