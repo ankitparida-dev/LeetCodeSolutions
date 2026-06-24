cd32b1dd10747ef2fa809d415629b9bfa8305f22
Ankit Parida <ankitparida386@gmail.com>
2026-06-24 08:22:14 +0530

    Time: 1 ms (99.98%), Space: 42.5 MB (63.17%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,14 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        int rev=0;
-        while(x!=0){
-            int b=x%10;
-            x/=10;
-            if(rev>214748364 || rev<-214748364){
-                return 0;
-            }
-            rev=rev*10+b;
+       int rev=0;
+       while(x!=0){
+        int b=x%10;
+        x/=10;
+        if(rev>214748364 || rev<-214748364){
+            return 0;
         }
-        return rev;
+        rev=rev*10+b;
+       }
+       return rev;
     }
 }
