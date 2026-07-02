a9bcbd3eb6a3fa8d4143241b72dfb4743b7c41a5
Ankit Parida <ankitparida386@gmail.com>
2026-07-02 10:44:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (18.14%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
@@ -1,14 +1,14 @@
 class Solution {
     
     public int countDigits(int num) {
-        int count=0,temp=num;
-       while(temp!=0){
+       int count=0,temp=num;
+       while(temp>0){
         int b=temp%10;
         temp/=10;
-         if(num%b==0){
+        if(num%b==0){
             count++;
         }
        }
-     return count;
+       return count;
     }
 }
