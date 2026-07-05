6873eca0de91013140a3775dd6724b076ff75f4a
Ankit Parida <ankitparida386@gmail.com>
2026-07-05 22:43:34 +0530

    Time: 20 ms (13.2%), Space: 50 MB (21.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map <String,List <String>> map=new HashMap();
-        for(String str:strs){
+         Map <String,List <String>> map=new HashMap();
+         for(String str:strs){
             int freq[]=new int[26];
             for(char ch:str.toCharArray()){
                 freq[ch-'a']++;
@@ -12,14 +12,14 @@ class Solution {
             }
             String key=sb.toString();
             if(map.containsKey(key)){
-                map.get(key).add(str);
+                 map.get(key).add(str);
             }
             else{
                 List <String> li=new ArrayList();
                 li.add(str);
                 map.put(key,li);
             }
-        }
-        return new ArrayList(map.values());
+         }
+         return new ArrayList(map.values());
     }
 }
