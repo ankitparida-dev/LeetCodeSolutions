b3c450ded6fb0d48d93c015bbc2abd5e927e3d1c
Ankit Parida <ankitparida386@gmail.com>
2026-07-08 11:49:23 +0530

    Time: 3 ms (72.42%), Space: 52.5 MB (65.95%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,62 +1,44 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-        int candidate1 = 0;
-        int candidate2 = 0;
-
-        int count1 = 0;
-        int count2 = 0;
-
-        // Find possible candidates
-        for(int num : nums) {
-
-            if(num == candidate1) {
+          int candidate1=0,candidate2=0,count1=0,count2=0;
+          for(int num:nums){
+              if(count1==0 && num!=candidate2){
+                candidate1=num;
                 count1++;
-            }
-            else if(num == candidate2) {
+              }
+              else if(count2==0 && num!=candidate1){
+                candidate2=num;
                 count2++;
-            }
-            else if(count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            }
-            else if(count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            }
-            else {
+              }
+              else if(num==candidate1){
+                     count1++;
+              }
+              else if(num==candidate2){
+                count2++;
+              }
+              else{
                 count1--;
                 count2--;
-            }
-        }
-
-        // Verify candidates
-        List<Integer> result = new ArrayList<>();
-
-        count1 = 0;
-        count2 = 0;
-
-        for(int num : nums) {
-            if(num == candidate1) {
+              }
+          }
+          count1=0;
+          count2=0;
+          for(int num:nums){
+            if(num==candidate1){
                 count1++;
             }
-            else if(num == candidate2) {
+            else if(num==candidate2){
                 count2++;
             }
-        }
-
-        int n = nums.length;
-
-        if(count1 > n/3) {
-            result.add(candidate1);
-        }
-
-        if(count2 > n/3) {
-            result.add(candidate2);
-        }
-
-        return result;
+          }
+          List <Integer> list=new ArrayList();
+          int n=nums.length;
+          if(count1>n/3){
+            list.add(candidate1);
+          }
+          if(count2>n/3){
+            list.add(candidate2);
+          }
+       return list;
     }
 }
