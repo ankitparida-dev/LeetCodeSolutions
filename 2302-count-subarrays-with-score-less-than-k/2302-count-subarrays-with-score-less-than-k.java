1cf4434048369d8c5a17b1d20e477c1adc41d1ba
Ankit Parida <ankitparida386@gmail.com>
2026-07-14 12:31:23 +0530

    Time: 2 ms (100%), Space: 87.9 MB (73.58%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+
+        long sum = 0;
+        long count = 0;
+
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            sum += nums[right];
+
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
