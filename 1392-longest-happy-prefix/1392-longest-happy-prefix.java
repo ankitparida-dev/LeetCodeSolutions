dab42b8b7bb4f27eebcd677a5ddf456cd9f43f68
Ankit Parida <ankitparida386@gmail.com>
2026-07-22 06:23:12 +0530

    Time: 9 ms (96.89%), Space: 47.4 MB (89.84%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int i = 1;
+        int len = 0;
+        while(i < n) {
+            if(s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if(len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        int happyLength = lps[n - 1];
+
+        return s.substring(0, happyLength);
+    }
+}
