7d3dd5ace5ad766c57f21a52ffde946ccaa4aa25
Ankit Parida <ankitparida386@gmail.com>
2026-07-30 09:33:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (11.82%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int reverse(int num,int rev){
-        if(num==0){
+    public static int reverse(int n,int rev){
+        if(n==0){
             return rev;
         }
-        return reverse(num/10,rev*10+num%10);
+        return reverse(n/10,rev*10+n%10);
     }
     public boolean isSameAfterReversals(int num) {
-        int rev=reverse(num,0);
-        int drev=reverse(rev,0);
-        return drev==num;
+       int rev=reverse(num,0);
+       int drev=reverse(rev,0);
+       return num==drev;
     }
 }
