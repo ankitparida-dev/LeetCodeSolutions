89d647cf044537122bcf404567b9b6a8cc5fd002
Ankit Parida <ankitparida386@gmail.com>
2026-08-03 11:27:08 +0530

    Time: 1 ms (26.57%), Space: 46.9 MB (11.96%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -5,7 +5,7 @@ class Solution {
        int matrix2[][]=new int[cols][rows];
        for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-           matrix2[j][i]=matrix[i][j];
+            matrix2[j][i]=matrix[i][j];
         }
        }
        return matrix2;
