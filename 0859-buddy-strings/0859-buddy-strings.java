0db238cce4204ec2e35817fc8dcdd7d35e9a24a2
Ankit Parida <ankitparida386@gmail.com>
2026-08-04 12:34:01 +0530

    Time: 1 ms (99.54%), Space: 43.8 MB (65.39%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        if (s.equals(goal)) {
+            // Need duplicate character for a valid swap
+            boolean[] seen = new boolean[26];
+
+            for (char ch : s.toCharArray()) {
+                if (seen[ch - 'a']) {
+                    return true;
+                }
+                seen[ch - 'a'] = true;
+            }
+
+            return false;
+        }
+
+        int first = -1;
+        int second = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) != goal.charAt(i)) {
+
+                if (first == -1) {
+                    first = i;
+                } else if (second == -1) {
+                    second = i;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return second != -1 &&
+               s.charAt(first) == goal.charAt(second) &&
+               s.charAt(second) == goal.charAt(first);
+    }
+}
