b03581f73613ed790bd9aabe137388b3c59d67ce
Ankit Parida <ankitparida386@gmail.com>
2026-08-04 11:23:18 +0530

    Time: 15 ms (19.38%), Space: 47.7 MB (10.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,20 @@
 class Solution {
     public String reverse(String s){
-        return new StringBuilder(s).reverse().toString();
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
     }
     public boolean isPalindrome(String s) {
         s=s.toLowerCase().replaceAll("[^a-z0-9]","");
