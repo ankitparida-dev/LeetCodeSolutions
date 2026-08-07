6c641ca0ca42cccd2d8b6ec422dfaf6b72d8b658
Ankit Parida <ankitparida386@gmail.com>
2026-08-07 10:50:51 +0530

    Time: 1 ms (99.9%), Space: 45.6 MB (32.87%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,5 +1,27 @@
 class Solution {
     public int compress(char[] chars) {
+        // int len=0;
+        // int i=0;
+        // int n=chars.length;
+        // while(i<n){
+        //     int count=0;
+        //     char ch=chars[i];
+        //     while(i<n && chars[i]==ch){
+        //         count++;
+        //         i++;
+        //     }
+        //     chars[len]=ch;
+        //     len++;
+        //     if(count>1){
+        //         String s=Integer.toString(count);
+        //         for(char c:s.toCharArray()){
+        //              chars[len]=c;
+        //              len++;
+        //         }
+        //     }
+        // }
+        // return len
+        
         int len=0;
         int i=0;
         int n=chars.length;
@@ -15,8 +37,8 @@ class Solution {
             if(count>1){
                 String s=Integer.toString(count);
                 for(char c:s.toCharArray()){
-                     chars[len]=c;
-                     len++;
+                    chars[len]=c;
+                    len++;
                 }
             }
         }
