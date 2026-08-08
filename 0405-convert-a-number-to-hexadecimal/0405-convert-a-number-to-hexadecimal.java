d6dfa005845df2d9c65808e487510cfae9f9df0f
Ankit Parida <ankitparida386@gmail.com>
2026-08-08 11:21:56 +0530

    Time: 6 ms (1.12%), Space: 48.9 MB (10.83%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,38 +1,23 @@
 class Solution {
-
-    private void helper(long n, StringBuilder sb) {
-
+    public String helper(long n) {
         if(n == 0) {
-            return;
+            return "";
         }
-
-        helper(n / 16, sb);
-
         long rem = n % 16;
-
         if(rem < 10) {
-            sb.append(rem);
-        } else {
-            sb.append((char)('a' + rem - 10));
+            return helper(n / 16) + rem;
         }
+        return helper(n / 16) + (char)('a' + rem - 10);
     }
 
     public String toHex(int num) {
-
         if(num == 0) {
             return "0";
         }
-
         long n = num;
-
-        if(n < 0) {
-            n += (1L << 32);
+         if(n < 0) {
+            n += (1L << 32); // convert to unsigned 32-bit value
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        helper(n, sb);
-
-        return sb.toString();
+        return helper(n);
     }
 }
