a24a43de7ae9f6509bba386360beda96e9f35e5d
Ankit Parida <ankitparida386@gmail.com>
2026-08-09 12:28:06 +0530

    Time: 17 ms (50.56%), Space: 51.2 MB (50.74%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,24 +1,18 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-       int n = matrix.length;
-        PriorityQueue<int[]> pq =
-            new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        for(int i = 0; i < n; i++) {
-            pq.offer(new int[]{matrix[i][0], i, 0});
-        }
-        while(--k > 0) {
-            int[] cur = pq.poll();
-            int row = cur[1];
-            int col = cur[2];
-            if(col + 1 < n) {
-                pq.offer(new int[]{
-                    matrix[row][col + 1],
-                    row,
-                    col + 1
-                });
-            }
-        }
-        return pq.peek()[0];
+       int n=matrix.length;
+       PriorityQueue <int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+       for(int i=0;i<n;i++){
+        pq.offer(new int[]{matrix[i][0],i,0});
+       }
+       while(--k>0){
+          int curr[]=pq.poll();
+          int row=curr[1];
+          int col=curr[2];
+          if(col+1<n){
+            pq.offer(new int[]{matrix[row][col+1],row,col+1});
+          }
+       }
+       return pq.peek()[0];
     }
 }
