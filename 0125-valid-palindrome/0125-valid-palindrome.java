7d7478719b008a65a89419a14066dea5023c5a76
Ankit Parida <ankitparida386@gmail.com>
2026-08-13 21:11:04 +0530

    Time: 14 ms (28.81%), Space: 47.3 MB (13.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public String reverse(String s){
-        return new StringBuilder(s).reverse().toString();
+        StringBuilder sb=new StringBuilder(s);
+        return sb.reverse().toString();
     }
     public boolean isPalindrome(String s) {
         s=s.toLowerCase().replaceAll("[^a-z0-9]","");
