bcc12c196d5a1cb7f277f417e438f021576b9750
Ankit Parida <ankitparida386@gmail.com>
2026-08-17 12:09:06 +0530

    Time: 9 ms (95.95%), Space: 47 MB (24.97%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,23 +1,17 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-       int ans=0;
-       while(truckSize>0){
-        int max=0;
-        int index=-1;
-        for(int i=0;i<boxTypes.length;i++){
-            if(boxTypes[i][0]>0 && boxTypes[i][1]>max){
-                max=boxTypes[i][1];
-                index=i;
-            }
-        }
-        if(index==-1){
+       Arrays.sort(boxTypes,(a,b)->b[1]-a[1]);
+       int total=0;
+       for(int[] ele:boxTypes){
+        int box=ele[0];
+        int unit=ele[1];
+        int min=Math.min(box,truckSize);
+        total+=min*unit;
+        truckSize=truckSize-min;
+        if(truckSize==0){
             break;
         }
-        int take=Math.min(boxTypes[index][0],truckSize);
-        ans+=take*boxTypes[index][1];
-        boxTypes[index][0]-=take;
-        truckSize-=take;
        }
-       return ans;
+       return total;
     }
 }
