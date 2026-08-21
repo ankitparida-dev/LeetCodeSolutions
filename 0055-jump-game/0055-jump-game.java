94fcb5ba9b02e17c621a73de31dc723a499352f6
Ankit Parida <ankitparida386@gmail.com>
2026-08-21 19:46:46 +0530

    Time: 2 ms (88.9%), Space: 47.8 MB (81.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean canJump(int[] nums) {
        int maxReach=0;
        for(int i=0;i<nums.length;i++){
-          if(i>maxReach){
+        if(i>maxReach){
             return false;
-          }
-          maxReach=Math.max(maxReach,i+nums[i]);
-          if(maxReach>=nums.length-1){
+        }
+        maxReach=Math.max(maxReach,i+nums[i]);
+        if(maxReach>=nums.length-1){
             return true;
-          }
+        }
        }
        return false;
     }
