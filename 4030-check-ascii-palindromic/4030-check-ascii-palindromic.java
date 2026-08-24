8f3f89cda221a3f92048d95a4cb239ea6b015f07
Ankit Parida <ankitparida386@gmail.com>
2026-08-24 22:18:53 +0530

    Time: 1 ms (100%), Space: 43.4 MB (100%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isPalindromic(String s) {
-        int left=0,right=s.length()-1;
-        while(left<=right){
-           
-            char a=s.charAt(left);
-            char b=s.charAt(right);
-            if(!(matches(a,b))){
-                return false;
-            }
-            left++;
-            right--;
+       int left=0,right=s.length()-1;
+       while(left<=right){
+        char a=s.charAt(left);
+        char b=s.charAt(right);
+        if(!matches(a,b)){
+            return false;
         }
-        return true;
+        left++;
+        right--;
+       }
+       return true;
     }
-    public boolean matches(char a,char b){
-        if(a=='f' && b=='f' || a=='n' && b=='v' || a=='v'&& b=='n'){
-            return true;
-        }
-        else{
-        return false;
-        }
-    }
+   public boolean matches(char a,char b){
+    return (a=='n' && b=='v' || a=='f' && b=='f' || a=='v' && b=='n');
+   }
 }
