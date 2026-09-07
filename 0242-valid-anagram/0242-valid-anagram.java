4aea384c9aa43e18a5dd64a2487a204ac8492de3
Ankit Parida <ankitparida386@gmail.com>
2026-09-07 22:14:48 +0530

    Time: 4 ms (83.67%), Space: 46.7 MB (16.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       if(s.length()!=t.length()){
-        return false;
-       }
-       char[] b=s.toCharArray();
-       char[] a=t.toCharArray();
-       Arrays.sort(b);
-       Arrays.sort(a);
-       String s1=String.valueOf(b);
-       String s2=String.valueOf(a);
-       return s1.equals(s2);
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char a[]=s.toCharArray();
+        char b[]=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
     }
 }
