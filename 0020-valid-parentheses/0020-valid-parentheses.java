b2f277ee7cc6318813ca5d3d9a00b19183f17981
Ankit Parida <ankitparida386@gmail.com>
2026-09-09 21:05:09 +0530

    Time: 4 ms (36.41%), Space: 43 MB (92.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,10 +3,10 @@ class Solution {
         Stack <Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            if(c=='(' || c=='{' || c=='['){
+            if(c=='{' || c=='[' ||c=='('){
                 stack.push(c);
             }
-            else if(c==')'||c=='}'||c==']'){
+            else if(c=='}' || c==']' ||c==')'){
                 if(stack.isEmpty()){
                     return false;
                 }
