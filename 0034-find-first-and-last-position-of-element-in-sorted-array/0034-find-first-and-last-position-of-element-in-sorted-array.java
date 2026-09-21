7bee9197cd2e9365bb5e07f8ba13c4024bf2a7d5
Ankit Parida <ankitparida386@gmail.com>
2026-09-21 09:50:28 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,12 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-      int result1=firstOccurence(nums,target);
-      int result2=lastOccurence(nums,target);
-      return new int[]{result1,result2};
-    }
-    public static int firstOccurence(int []nums,int target){
+    public static int firstOccurence(int[] nums,int target){
         int low=0,high=nums.length-1;
         int result=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
-               result= mid;
-                high=mid-1;
+              result=mid;
+              high=mid-1;
             }
             else if(nums[mid]<target){
                 low=mid+1;
@@ -20,16 +15,16 @@ class Solution {
                 high=mid-1;
             }
         }
-        return result;
+       return result;
     }
-     public static int lastOccurence(int []nums,int target){
+    public static int lastOccurence(int[] nums,int target){
         int low=0,high=nums.length-1;
         int result=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
-                result= mid;
-                low=mid+1;
+              result=mid;
+              low=mid+1;
             }
             else if(nums[mid]<target){
                 low=mid+1;
@@ -38,6 +33,11 @@ class Solution {
                 high=mid-1;
             }
         }
-        return result;
+       return result;
+    }
+    public int[] searchRange(int[] nums, int k) {
+        int res1=firstOccurence(nums,k);
+        int res2=lastOccurence(nums,k);
+        return new int[]{res1,res2};
     }
 }
