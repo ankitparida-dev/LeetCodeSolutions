56bf42cb64c191003563a1406b669b4df9b70c5b
Ankit Parida <ankitparida386@gmail.com>
2026-09-22 12:48:33 +0530

    Time: 4 ms (73.56%), Space: 45.3 MB (35.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       int n=nums2.length;
-       int[] nge=new int[n];
-       Arrays.fill(nge,-1);
-       Stack<Integer> stack=new Stack<>();
-       for(int i=0;i<n;i++){
-        while(!stack.isEmpty()&&nums2[i]>nums2[stack.peek()]){
-            int idx=stack.pop();
-            nge[idx]=nums2[i];
+        Map <Integer,Integer> map=new HashMap<>();
+        Stack <Integer> st=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i]>=st.peek()){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                map.put(nums2[i],-1);
+            }
+            else{
+                map.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
         }
-        stack.push(i);
-       }
-       int[] ans=new int[nums1.length];
-       for(int i=0;i<nums1.length;i++){
-        for(int j=0;j<n;j++){
-            if(nums2[j]==nums1[i]){
-                ans[i]=nge[j];
-                break;
+        int res[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])){
+                res[i]=map.get(nums1[i]);
             }
         }
-       }
-       return ans;
+        return res;
     }
 }
