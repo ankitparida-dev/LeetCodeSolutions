2d0ea0e6007154370adc9fde596a8d743f9d60d8
Ankit Parida <ankitparida386@gmail.com>
2026-09-25 10:33:56 +0530

    Time: 1 ms (9.23%), Space: 47.3 MB (92.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,22 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;  
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }    
-        return power(x, N);
-    }
-
-    private double power(double x, long n) {
-        if (n == 0) return 1;
-
-        double half = power(x, n / 2);
-
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return x * half * half;
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
         }
+        double ans=1;
+        while(N>0){
+            if(N%2==1){
+                ans*=x;
+            }
+            x*=x;
+            N/=2;
+        }
+        return ans;
     }
 }
